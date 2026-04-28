06ae0310d2d4742c497327132f96f90364aaad53
ArturoMD7 <arturomtzdel17@gmail.com>
2026-04-28 16:18:46 -0600

    ui version 2 correccion

--- backend/ocr_service.py
+++ backend/ocr_service.py
@@ -1,14 +1,12 @@
-import numpy as np
-from paddleocr import PaddleOCR
+import pytesseract
 from pdf2image import convert_from_path
 from docx import Document
 
 def procesar_pdf(input_pdf, output_word, pag_inicio=1, pag_fin=None):
-    ocr = PaddleOCR(use_angle_cls=True, lang='es')
 
     pages = convert_from_path(
         input_pdf,
-        dpi=200,
+        dpi=150,
         first_page=pag_inicio,
         last_page=pag_fin
     )
@@ -16,14 +14,8 @@ def procesar_pdf(input_pdf, output_word, pag_inicio=1, pag_fin=None):
     texto_final = ""
 
     for page in pages:
-        img = np.array(page)
-        result = ocr.ocr(img, cls=True)
-
-        if result and result[0]:
-            for line in result[0]:
-                texto_final += line[1][0] + "\n"
-
-        texto_final += "\n\n"
+        texto = pytesseract.image_to_string(page, lang='spa')
+        texto_final += texto + "\n\n"
 
     doc = Document()
     for p in texto_final.split("\n"):
